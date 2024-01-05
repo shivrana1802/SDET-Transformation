62fc02bcd50d654e8e22374b5cc4e3d35db89b4b
shivrana1802 <108927199+shivrana1802@users.noreply.github.com>
2024-01-05 12:39:08 +0530

    javascript fundamentals exercise

---
+++ JavaScript Fundamentals Part 1/Exercise 1.js
@@ -0,0 +1,16 @@
+// JavaScript Basics - Variables, Values, Data types, Basic Operators
+
+// Problem - Swap 2 numbers with third variable
+
+// Solution 
+
+
+let x = 20;
+console.log(x);
+let y = 50;
+console.log(y);
+let z = x ;
+x = y;
+y = z; 
+console.log(x,y);
+
---
+++ JavaScript Fundamentals Part 1/Exercise 2.js
@@ -0,0 +1,25 @@
+// JavaScript Basics - Strings and Template Literals,  Basic Operators
+
+// Problem - Swap 2 numbers without third variable using Strings and Template Literals
+
+// Solution 
+
+let x = 30;
+let y = 40;
+console.log(`value of x is ${x} and value of y is ${y}`);
+
+x = x+y;  
+y = x-y; 
+x = x-y;
+console.log(`value of x is ${x} and value of y is ${y}`);
+
+// Basic Arithmetic operators
+
+x = 20;
+console.log(x);
+x+=10;
+console.log(x);
+x=+20;
+console.log(x);
+
+console.log(`value of x is ${x}`);
